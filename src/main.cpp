020fef897a12091c8ecc2ccd09ca563b0d2a4a05
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-06-25 22:05:17 +0200

    Add compass calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <LSM303.h>
 
 #define PIN_MAG_PLUS 23
@@ -10,16 +11,45 @@
 
 LSM303 compass;
 
+/*
+Calibrate compass
+Run this function in loop and spin hat in all directions to get min/max
+magnetometer values In future, it would be nice to save those values in eeprom
+and only update them once in a while
+*/
+void autoCalibrate() {
+    compass.m_min.x = min(compass.m_min.x, compass.m.x);
+    compass.m_min.y = min(compass.m_min.y, compass.m.y);
+    compass.m_min.z = min(compass.m_min.z, compass.m.z);
+
+    compass.m_max.x = max(compass.m_max.x, compass.m.x);
+    compass.m_max.y = max(compass.m_max.y, compass.m.y);
+    compass.m_max.z = max(compass.m_max.z, compass.m.z);
+}
+
 void setup() {
     Serial.begin(115200);
+    Serial.println("Initializing Hatlight...");
+    Serial.println("Init compass...");
     pinMode(PIN_MAG_MINUS, OUTPUT);
     pinMode(PIN_MAG_PLUS, OUTPUT);
     digitalWrite(PIN_MAG_MINUS, 0);
     digitalWrite(PIN_MAG_PLUS, 1);
     delay(1);
 
+    Wire.begin(PIN_MAG_SDA, PIN_MAG_SCL);
     compass.init(LSM303::device_DLHC);
     compass.enableDefault();
+    compass.read();
+    Serial.println("Compass working");
+
+    Serial.println("Going to loop...");
 }
 
-void loop() {}
+void loop() {
+    compass.read();
+    // Run calibration for first 45 seconds
+    if (millis() < 45 * 1000) {
+        autoCalibrate();
+    }
+}
