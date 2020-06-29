623ce59c75dd5a870a76d6fe05dfd576fdb322aa
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-06-29 21:13:45 +0200

    I'm not sure what happened here I just:  - copy what is in the LSM303 polulu library  - cut off half of it (like, literally)  - paste it here using Y and Z values because the magnetometer is in vertical position  - do classic atan2 stuff  - add 180 degrees because it was showing the opposite direction And like... it just fucking works. Except for tilting too much in one direction, and of course not perfectly. I don't know why people in polulu did the second half in their lib.
    If you are anyone in future, who examinates why this code looks this way, I think you know by now, that I have no idea how this works, nor is there any good and straightforward source in internet about it.
    I don't know why the fuck no, this is so common problem... right?
    
    Thanks for reading ^-^

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,11 @@ Run this function in loop and spin hat in all directions to get min/max
 magnetometer values In future, it would be nice to save those values in eeprom
 and only update them once in a while
 */
+
+LSM303::vector<int16_t> _janusz_calibration_min = {-578, -586, -425},
+                        _janusz_calibration_max = {613, 608, 542};
+LSM303::vector<int16_t> _calib_place_min = {32767, 32767, 32767},
+                        _calib_place_max = {-32768, -32768, -32768};
 void autoCalibrate() {
     compass.m_min.x = min(compass.m_min.x, compass.m.x);
     compass.m_min.y = min(compass.m_min.y, compass.m.y);
@@ -28,10 +33,36 @@ void autoCalibrate() {
     compass.m_max.z = max(compass.m_max.z, compass.m.z);
 }
 
-// This is for logger to end every log with \n
-void _printNewline(Print* _logOutput) {
-  _logOutput->print('\n');
+// This is TODO in future, if I every finally find method to get true heading
+// that just FUCKING WORKS
+// ...
+// few tears and mental breakdowns later:
+// Does this...
+// Does this just FUCKING WORK?????? 
+float getHeadingAzimuth(){
+    compass.read();
+    // This is basically copy of what is in the library, but cut off few things
+
+    LSM303::vector<int32_t> temp_m = {compass.m.x, compass.m.y, compass.m.z};
+    // subtract offset (average of min and max) from magnetometer readings
+    temp_m.x -= ((int32_t)compass.m_min.x + compass.m_max.x) / 2;
+    temp_m.y -= ((int32_t)compass.m_min.y + compass.m_max.y) / 2;
+    temp_m.z -= ((int32_t)compass.m_min.z + compass.m_max.z) / 2;
+
+    LSM303::vector<float> E;
+    LSM303::vector_cross(&temp_m, &compass.a, &E);
+    
+    float head = atan2(E.y, E.z) * 180 / PI;
+    head += 180;
+    if (head < 0){
+        head += 360;
+    }
+
+    return head;
 }
+
+// This is for logger to end every log with \n
+void _printNewline(Print* _logOutput) { _logOutput->print('\n'); }
 
 void setup() {
     Serial.begin(115200);
@@ -43,7 +74,7 @@ void setup() {
     pinMode(PIN_MAG_PLUS, OUTPUT);
     digitalWrite(PIN_MAG_MINUS, 0);
     digitalWrite(PIN_MAG_PLUS, 1);
-    delay(1);
+    delay(10);
 
     Wire.begin(PIN_MAG_SDA, PIN_MAG_SCL);
     compass.init(LSM303::device_DLHC);
@@ -54,14 +85,14 @@ void setup() {
     } else {
         Log.trace("Compass working");
     }
+    compass.m_max = _janusz_calibration_max;
+    compass.m_min = _janusz_calibration_min;
 
     Log.notice("Setup done, ging to loop...");
 }
 
 void loop() {
     compass.read();
-    // Run calibration for first 45 seconds
-    if (millis() < 45 * 1000) {
-        autoCalibrate();
-    }
+    Log.notice("%F", getHeadingAzimuth());
+    delay(50);
 }
